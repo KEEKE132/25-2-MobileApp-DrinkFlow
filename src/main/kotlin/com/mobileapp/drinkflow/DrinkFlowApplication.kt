b585e61b549e3ffc9e5a3e6ee95b3d6ae3f4b41f
KEEKE132 <dksvlfdhs1@gmail.com>
2025-11-03 22:08:01 +0900

    Initial commit

---
+++ src/main/kotlin/com/mobileapp/drinkflow/DrinkFlowApplication.kt
@@ -0,0 +1,11 @@
+package com.mobileapp.drinkflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DrinkFlowApplication
+
+fun main(args: Array<String>) {
+    runApplication<DrinkFlowApplication>(*args)
+}
---
+++ src/test/kotlin/com/mobileapp/drinkflow/DrinkFlowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mobileapp.drinkflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DrinkFlowApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
